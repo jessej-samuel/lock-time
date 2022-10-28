594c441471f16605461026d11a79c0475c61238a
jessej-samuel <1234jessej@gmail.com>
2022-10-28 21:36:26 +0530

    tweak: minor ui changes

--- components/app/Todo.js
+++ components/app/Todo.js
@@ -5,7 +5,7 @@ const Todo = ({ task }) => {
   return (
     <div
       className={
-        "w-full max-w-[27rem] bg-background border rounded px-4 py-2 hover:shadow-sm shadow-black group h-20"
+        "w-full max-w-[27rem] bg-background border rounded px-4 py-2 hover:shadow-md shadow-black group h-20 transition-all duration-300 ease-in-out hover:scale-y-105"
       }
     >
       <h2 className="font-medium text-lg" title={task.title}>{task.title.length > 20 ? `${task.title.slice(0,20)}...` : task.title}</h2>
@@ -22,7 +22,7 @@ const Todo = ({ task }) => {
           ? "Due tomorrow"
           : daysLeft + " days left"}
       </p>
-      <div className="group-hover:flex border rounded-full w-fit bg-primary/10 cursor-pointer hidden text-xs font-semibold text-black/70">
+      <div className="group-hover:flex border rounded-full w-fit bg-primary/10 cursor-pointer hidden text-xs font-semibold text-black/70 transition-all duration-300 ease-in-out">
         <div className="px-2 py-1 hover:bg-primary/10 rounded-full hover:bg-red-600 hover:text-white/75">🗑️ Delete</div>
         <div className="px-2 py-1 border-x  hover:bg-primary/10 rounded-full hover:bg-blue-600 hover:text-white/75">✏️ Edit</div>
         <div className="px-2 py-1  hover:bg-primary/10 rounded-full hover:bg-green-600 hover:text-white/75">✅ Done</div>
