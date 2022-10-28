1db8db703e0b8f734181479ab90c2c673d29c0a4
jessej-samuel <1234jessej@gmail.com>
2022-10-28 21:36:42 +0530

    feat: added a landing page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <p>Hello World</p>;
+  return (
+    <>
+      <p>Hello! Want to become more productive?</p>
+      <Link href={"/app"}>
+        <a className="text-blue-500 hover:underline">Get started!</a>
+      </Link>
+    </>
+  );
 }
